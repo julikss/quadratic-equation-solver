de8aa3a3d43800708a1ee1e09884c8470c0dab68
julikss <julie.lehenka@gmail.com>
2022-02-18 22:58:45 +0200

    Added interactive mode
    A mode can be chosen by entering a digit to the console('1' or '0').
    At this point there are functions interactiveMode() for displaying
    interactive mode and checkInput() for validation of input data.
    Reading data from the console is provided by using the readline module.

--- main.js
+++ main.js
@@ -1,5 +1,53 @@
 'use strict';
 
+const readline = require('readline');
+
+const rl = readline.createInterface({ 
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const coef = [];
+
+rl.question('Choose mode: for interactice press "0", for file mode - "1"...\n', num => {
+    if (num === '0') interactiveMode();
+    else if (num === '1') console.log('will be soon...');
+    else console.log('You entered invalid number! Try again')
+})
+
+const interactiveMode = () => {
+    console.log('Please insert coefficients:'); 
+    askFunc();
+}
+ 
+const askFunc = () => {
+    rl.question('a = ', a => {
+        checkInput(a);
+        rl.question('b = ', b => {
+            checkInput(b);
+            rl.question('c = ', c => {
+                checkInput(c);
+            });
+        });
+    });    
+}
+
+const checkInput = arg => {
+    if (isNaN(arg)) {
+        console.log(`Error. Expected a valid real number, got ${arg} instead`);
+    } else if (arg === '0' && coef.length === 0) {
+        console.log('Error! a cannot be 0!');
+        askFunc();
+    } else {
+        arg = +arg;
+        coef.push(arg);
+    }
+
+    if (coef.length === 3) {
+        rl.close();
+        solver(coef);
+    }
+}
 const solver = ([...args]) => {
     let x1;
     let x2;
@@ -24,5 +72,3 @@ const solver = ([...args]) => {
         console.log(`There are two roots:\nx1 = ${x1}\nx2 = ${x2}`)
     }
 }
-
-solver([1,2,3]);
