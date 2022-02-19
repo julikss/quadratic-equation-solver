2cd472ffee421b0b693fb9b5e23a8309e12bab1b
julikss <julie.lehenka@gmail.com>
2022-02-19 16:43:05 +0200

    Added file mode
    This mode is made to solve equation using a txt file.
    Made function fileMode() to check data from file (test.txt
    is for testing program) without proper checking for incorrect
    coefficients.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({ 
     input: process.stdin,
@@ -11,8 +12,11 @@ const coef = [];
 
 rl.question('Choose mode: for interactice press "0", for file mode - "1"...\n', num => {
     if (num === '0') interactiveMode();
-    else if (num === '1') console.log('will be soon...');
-    else console.log('You entered invalid number! Try again')
+    else if (num === '1') fileMode();
+    else {
+        console.log('You entered invalid number!');
+        rl.close();
+    }
 })
 
 const interactiveMode = () => {
@@ -20,6 +24,24 @@ const interactiveMode = () => {
     askFunc();
 }
  
+const fileMode = () => {
+    rl.question(
+        `Please enter the name of txt file with coefficients (e.g. test.txt)\nIt should contain information like this: 1 0 0\n`,
+         fileName => {
+             if (fs.existsSync(fileName)) {
+                const data = fs.readFileSync(fileName, 'utf8');
+                const example = /-*^[1-9]\d+[.\d+]* -*\d+[.\d+]* -*\d+[.\d+]*/;
+                if(example.test(data)) {
+                    const sortedData = data.split(' ');
+                    for(const el of sortedData) {
+                        coef.push(parseFloat(el));
+                    };
+                    solver(coef);
+                } else console.log('incorrect data')
+             }
+         })
+}
+
 const askFunc = () => {
     rl.question('a = ', a => {
         checkInput(a);
